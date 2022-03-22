252d705250feb02f7814bd6d0c5b2df8076565c3
a86356 <100000356@qq.com>
2022-03-22 19:24:01 +0800

    1
    2

--- src/views/b.vue
+++ src/views/b.vue
@@ -1,67 +1,5 @@
 <template>
-  <a-layout style="min-height: 100vh">
-    <a-layout-sider v-model:collapsed="collapsed" collapsible>
-      <div class="logo" />
-      <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="inline">
-        <a-menu-item key="1">
-          <pie-chart-outlined />
-          <span>我的参数</span>
-        </a-menu-item>
-
-        <a-menu-item key="2">
-          <desktop-outlined />
-          <span>Option 2</span>
-        </a-menu-item>
-
-        <a-sub-menu key="sub1">
-          <template #title>
-            <span>
-              <user-outlined />
-              <span>User</span>
-            </span>
-          </template>
-          <a-menu-item key="3">Tom</a-menu-item>
-          <a-menu-item key="4">Bill</a-menu-item>
-          <a-menu-item key="5">Alex</a-menu-item>
-        </a-sub-menu>
-
-        <a-sub-menu key="sub2">
-          <template #title>
-            <span>
-              <team-outlined />
-              <span>Team</span>
-            </span>
-          </template>
-          <a-menu-item key="6">Team 1</a-menu-item>
-          <a-menu-item key="8">Team 2</a-menu-item>
-        </a-sub-menu>
-
-        <a-menu-item key="9">
-          <file-outlined />
-          <span>File</span>
-        </a-menu-item>
-      </a-menu>
-    </a-layout-sider>
-
-    <a-layout>
-      <a-layout-header style="background: #fff; padding: 0" />
-
-      <a-layout-content style="margin: 0 16px">
-        <a-breadcrumb style="margin: 16px 0">
-          <a-breadcrumb-item>User</a-breadcrumb-item>
-          <a-breadcrumb-item>Bill</a-breadcrumb-item>
-        </a-breadcrumb>
-        <div
-          :style="{ padding: '24px', background: '#fff', minHeight: '360px' }"
-        >
-          Bill is a cat11111.
-        </div>
-      </a-layout-content>
-      <a-layout-footer style="text-align: center">
-        Ant Design ©2018 Created by Ant UED
-      </a-layout-footer>
-    </a-layout>
-  </a-layout>
+  <div id="main" style="width: 400px; height: 400px"></div>
 </template>
 
 <script lang="ts">
@@ -69,6 +7,7 @@ import { defineComponent, toRefs, reactive, onMounted, ref } from "vue";
 import { useCounterStore } from "@/store/counter";
 import { useRoute } from "vue-router";
 import { login } from "@/service/home";
+import * as echarts from "echarts";
 
 export default defineComponent({
   setup() {
@@ -82,6 +21,34 @@ export default defineComponent({
     onMounted(async () => {
       const p = await login({ username: "12312", password: "2311" });
       state.title = p.data.username;
+
+      // 基于准备好的dom，初始化echarts实例
+      const dom: any = document.getElementById("main");
+      const myChart = echarts.init(dom);
+
+      // 绘制图表
+      myChart.setOption({
+        title: {
+          text: "ECharts 入门示例",
+        },
+        tooltip: {},
+        xAxis: {
+          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+        },
+        yAxis: {},
+        series: [
+          {
+            name: "销量",
+            type: "bar",
+            data: [15, 20, 36, 10, 10, 20],
+          },
+          {
+            name: "价格",
+            type: "bar",
+            data: [51, 210, 326, 110, 140, 210],
+          },
+        ],
+      });
     });
 
     function handleTab() {
